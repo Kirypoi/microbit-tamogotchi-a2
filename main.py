3567258186f38f365e9936085072302ee89a84e1
Kirypoi <67997855+Kirypoi@users.noreply.github.com>
2022-12-02 16:47:28 +0700

    v2

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ def feedRecorder(counter:int) -> int:
 
 def feedACtionChangesToInputNumber():
     counter = 0
-    input.on_pin_pressed(TouchPin.P0, counter++)
-    
+    input.on_pin_pressed(TouchPin.P0, feedRecorder(counter))
+        
     if (counter > 0 & counter <= 4):
         feedToDefaultAction()
     if (counter > 4 & counter <= 8):
