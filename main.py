5772d880f843e885a0241ecb133bdc89f0cf28d8
Kirypoi <67997855+Kirypoi@users.noreply.github.com>
2022-12-08 13:04:21 +0700

    help

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
+def feed_to_overfed_action():
+    basic.show_leds("""
+        # . . . .
+                . # . . .
+                . . # . .
+                . # . # .
+                # . . . #
+    """)
+    return 0
+# When fed shows emotion
+def feed_to_default_action():
+    basic.show_leds("""
+        # # . . .
+        . # # . .
+        . . # . .
+        . . . # .
+        . . . . #
+    """)
+    return 0
 # bao
 def poop():
     pass
+
+def feed_action_changes_to_input_number(counter:int):
+    # basic.show_number(9)
+    if counter > 0 and counter <= 4:
+        feed_to_default_action()
+    if counter > 4 and counter <= 8:
+        feed_to_overfed_action()
+    if counter > 8:
+        basic.show_string("It's dead.")
+    
+def feed_input():
+    # input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+    input.on_pin_pressed(TouchPin.P0, feed_to_default_action)
+
+counter = 0
+def on_pin_pressed_p0():
+    feed_action_changes_to_input_number(counter)
+    counter = counter + 1
+    
 # bao
 def sing():
     pass
@@ -10,46 +48,10 @@ def play():
 # sleep
 def sleep():
     pass
-# When fed shows emotion
-def feedToDefaultAction():
-    basic.show_leds("""
-        # . . . .
-        . # . . .
-        . . # . .
-        . . . # .
-        . . . . #
-    """)
-    return 0
-
-def feedToOverFedAction():
-    basic.show_leds("""
-            # . . . .
-            . # . . .
-            . . # . .
-            . # . # .
-            # . . . #
-        """)
-    return 0
-
-def feedRecorder(counter:int) -> int:
-    result = counter
-    result += 1
-    return result
-
-def feedACtionChangesToInputNumber():
-    counter = 0
-    input.on_pin_pressed(TouchPin.P0, feedRecorder(counter))
-        
-    if (counter > 0 & counter <= 4):
-        feedToDefaultAction()
-    if (counter > 4 & counter <= 8):
-        feedToOverFedAction()
-    else: basic.show_string("It's dead.")
-    
-    return 0    
 
 def main():
-    feedACtionChangesToInputNumber()
+    feed_input()
+    # basic.show_number(9)
     return 0
 
 main()
--- main.ts
+++ main.ts
@@ -1,6 +1,57 @@
+function feed_to_overfed_action(): number {
+    basic.showLeds(`
+        # . . . .
+                . # . . .
+                . . # . .
+                . # . # .
+                # . . . #
+    `)
+    return 0
+}
+
+//  When fed shows emotion
+function feed_to_default_action(): number {
+    basic.showLeds(`
+        # # . . .
+        . # # . .
+        . . # . .
+        . . . # .
+        . . . . #
+    `)
+    return 0
+}
+
 //  bao
 function poop() {
     
+}
+
+function feed_action_changes_to_input_number(counter: number) {
+    //  basic.show_number(9)
+    if (counter > 0 && counter <= 4) {
+        feed_to_default_action()
+    }
+    
+    if (counter > 4 && counter <= 8) {
+        feed_to_overfed_action()
+    }
+    
+    if (counter > 8) {
+        basic.showString("It's dead.")
+    }
+    
+}
+
+function feed_input() {
+    //  input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+    input.onPinPressed(TouchPin.P0, feed_to_default_action)
+}
+
+let counter = 0
+function on_pin_pressed_p0() {
+    let counter: number;
+    feed_action_changes_to_input_number(counter)
+    counter = counter + 1
 }
 
 //  bao
@@ -18,37 +69,10 @@ function sleep() {
     
 }
 
-//  When fed shows emotion
-function feedToDefaultAction(): number {
-    basic.showLeds(`
-        # . . . .
-        . # . . .
-        . . # . .
-        . . . # .
-        . . . . #
-    `)
-    return 0
-}
-
-function feedToOverFedAction(): number {
-    basic.showLeds(`
-            # . . . .
-            . # . . .
-            . . # . .
-            . # . # .
-            # . . . #
-        `)
-    return 0
-}
-
-function feedRecorder(counter: () => void): () => void {
-    let result = counter
-    result += 1
-    return result
-}
-
 function main(): number {
-    feedACtionChangesToInputNumber
+    feed_input()
+    //  basic.show_number(9)
     return 0
 }
 
+main()
